b5d0a441277a882e82b5372b55aa19c32c741d73
Dalibor <dalibor@kulturniautomat.com>
2022-11-24 15:32:44 +0100

    test: verified behaviour when modifying an existing exchange rate

--- js/test_money.js
+++ js/test_money.js
@@ -4,7 +4,7 @@ const Portfolio = require("./portfolio");
 const Bank = require("./bank");
 
 class MoneyTest {
-  constructor() {
+  setUp() {
     this.bank = new Bank();
     this.bank.addExchangeRate("EUR", "USD", 1.2);
     this.bank.addExchangeRate("USD", "KRW", 1100);
@@ -64,18 +64,23 @@ class MoneyTest {
       portfolio.evaluate(this.bank, "Kalganid");
     }, expectedError);
   }
-  testConversion() {
-    let bank = new Bank();
-    bank.addExchangeRate("EUR", "USD", 1.2);
+  testConversionWithDifferentRatesBetweenTwoCurrencies() {
     let tenEuros = new Money(10, "EUR");
-    assert.deepStrictEqual(bank.convert(tenEuros, "USD"), new Money(12, "USD"));
+    assert.deepStrictEqual(
+      this.bank.convert(tenEuros, "USD"),
+      new Money(12, "USD")
+    );
+    this.bank.addExchangeRate("EUR", "USD", 1.3);
+    assert.deepStrictEqual(
+      this.bank.convert(tenEuros, "USD"),
+      new Money(13, "USD")
+    );
   }
   testConversionWithMissingExchangeRate() {
-    let bank = new Bank();
     let tenEuros = new Money(10, "EUR");
     let expectedError = new Error("EUR->Kalganid");
-    assert.throws(function () {
-      bank.convert(tenEuros, "Kalganid");
+    assert.throws(() => {
+      this.bank.convert(tenEuros, "Kalganid");
     }, expectedError);
   }
   getAllTestMethods() {
@@ -84,7 +89,15 @@ class MoneyTest {
     let testMethods = allProps.filter((p) => {
       return typeof moneyPrototype[p] === "function" && p.startsWith("test");
     });
-    return testMethods;
+    return randomizeTestOrder(testMethods);
+
+    function randomizeTestOrder(testMethods) {
+      for (let i = testMethods.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [testMethods[i], testMethods[j]] = [testMethods[j], testMethods[i]];
+      }
+      return testMethods;
+    }
   }
   runAllTests() {
     let testMethods = this.getAllTestMethods();
@@ -92,6 +105,7 @@ class MoneyTest {
       console.log("Running: %s()", m);
       let method = Reflect.get(this, m);
       try {
+        this.setUp();
         Reflect.apply(method, this, []);
       } catch (e) {
         if (e instanceof assert.AssertionError) {
